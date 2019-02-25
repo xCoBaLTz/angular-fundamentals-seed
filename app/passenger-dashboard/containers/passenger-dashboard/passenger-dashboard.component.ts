b9fe5574ab709f482685982b07c80ac4051a98bd
Seanan Chabra <seananchabra@hotmail.com>
2019-02-25 16:07:07 -0500

    created first service

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { PassengerDashboardService } from "../../passenger-dashboard.service"
 import { Passenger } from "../../models/passenger.interface";
+
 
 @Component({
     selector: 'passenger-dashboard',
@@ -14,38 +16,11 @@ import { Passenger } from "../../models/passenger.interface";
 })
 export class PassengerDashboardComponent implements OnInit{
   passengers: Passenger[];
+
+  constructor(private passengerService: PassengerDashboardService){}
+
   ngOnInit(){
-    this.passengers = [{
-      id: 1,
-      fullname:'Stephen',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      checkedInDate: null,
-      children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7}]
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkedInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkedInDate: 1488412800000,
-      children: [{name: 'Jessica', age: 1}]
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-      checkedInDate: null,
-      children: null
-    }];
+    this.passengers = this.passengerService.getPassengers();
   }
 
   handleRemove(event: Passenger){
