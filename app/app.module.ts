4a6091604c5146e03c8e12a1ceca8bdfd2d07857
Seanan Chabra <seananchabra@hotmail.com>
2019-02-27 15:18:33 -0500

    learned how to use hash location strategy

--- app/app.module.ts
+++ app/app.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     // angular modules
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     // custom modules
     PassengerDashboardModule
   ],
